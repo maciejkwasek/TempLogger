8c4596406f0ed3d6b8e21ce7094c49b71dc9196b
Maciej Kwasek <maciej.kwasek@elester-pkp.com.pl>
2023-12-16 23:16:55 +0100

    refactor CmdLogger

--- CmdLogger.cpp
+++ CmdLogger.cpp
@@ -10,6 +10,37 @@
 /*
  *
  */
+static void printRecord(const Logger_Record_t* record)
+{
+    Rtc_ClockState_t clk;
+    Rtc_UnpackTime(record->flagAndDt & 0x7fffffff, &clk);
+
+    if(record->flagAndDt & 0x80000000)
+    {
+        Serial.print("+;");
+    }
+    else
+    {
+        Serial.print("-;");
+    }
+
+    Serial.print(clk.year); Serial.print(".");
+    Serial.print(clk.month); Serial.print(".");
+    Serial.print(clk.day); Serial.print(";");
+
+    Serial.print(clk.hour); Serial.print(":");
+    Serial.print(clk.min); Serial.print(":");
+    Serial.print(clk.sec); Serial.print(";");
+
+    Serial.print(record->t /10.0); Serial.print(";");
+    Serial.print(record->rh); Serial.print(";");
+
+    Serial.println("");
+}
+
+/*
+ *
+ */
 void CmdLogger_GetRecords(const char* params)
 {
     UNUSED(params);
@@ -23,31 +54,7 @@ void CmdLogger_GetRecords(const char* params)
 
         if(index != LOGGER_DUMMYIDX)
         {
-            
-            Rtc_ClockState_t clk;
-            Rtc_UnpackTime(record.flagAndDt & 0x7fffffff, &clk);
-
-            if(record.flagAndDt & 0x80000000)
-            {
-                Serial.print("+;");
-            }
-            else
-            {
-                Serial.print("-;");
-            }
-
-            Serial.print(clk.year); Serial.print(".");
-            Serial.print(clk.month); Serial.print(".");
-            Serial.print(clk.day); Serial.print(";");
-
-            Serial.print(clk.hour); Serial.print(":");
-            Serial.print(clk.min); Serial.print(":");
-            Serial.print(clk.sec); Serial.print(";");
-
-            Serial.print(record.t /10.0); Serial.print(";");
-            Serial.print(record.rh); Serial.print(";");
-
-            Serial.println("");
+            printRecord(&record);
         }
 
     } while (index != LOGGER_DUMMYIDX);
